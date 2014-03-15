3bea7107370883bbb9123e98bcb5ddf6fa1b3455
Alan Gorton <dev.code1up@googlemail.com>
2014-03-15 14:00:57 +0000

    wip

--- client/router.js
+++ client/router.js
@@ -1,10 +1,15 @@
 Router.configure({
-  layoutTemplate: 'layout'
+  layoutTemplate: "layout"
 });
 
 Router.map(function () {
-  this.route('create', {
-    path: '/',
-    template: 'create'
+  this.route("create", {
+    path: "/",
+    template: "create"
+  });
+
+  this.route("items", {
+    path: "/items",
+    template: "items"
   });
 });
--- client/views/create.js
+++ client/views/create.js
@@ -2,10 +2,31 @@ Template.create.helpers({
     categories: function () {
         return Categories;
     },
+
+    items: function () {
+        return Items;
+    },
+    
     location: function () {
         navigator.geolocation.getCurrentPosition(function (position) {
             console.log(position.coords.latitude);
             console.log(position.coords.longitude);
         });
+    }
+});
+
+Template.create.events({
+    "click #addButton": function(e) {
+        e.preventDefault();
+
+        var category = document.getElementById("category").value;
+        var description = document.getElementById("description").value;
+
+        var item = {
+            category: category,
+            description: description
+        };
+
+        Items.insert(item);
     }
 });

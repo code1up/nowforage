956e5eb8fe0b986cc3fe285c7528bbe032963fb0
Alan Gorton <dev.code1up@googlemail.com>
2014-03-15 15:36:30 +0000

    wip

--- client/views/create.js
+++ client/views/create.js
@@ -1,3 +1,45 @@
+Template.create.created = function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        Session.set("position", position);
+    });
+};
+
+Template.create.rendered = function () {
+    if (this._alreadyCalled) {
+        return;
+    }
+
+    this._alreadyCalled = true;
+
+    var video = this.find("video");
+
+    navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    video.onloadedmetadata = function(e) {
+        console.log("onloadedmetadata");
+    };
+        
+    var options = {
+        video: true,
+        audio: false
+    };
+
+    navigator.getUserMedia(options, function (stream) {
+        // video.src = window.URL.createObjectURL(stream);
+        video.src = window.webkitURL.createObjectURL(stream);
+
+        video.play();
+
+    }, function(e) {
+        console.log("Video failed!");
+        console.log(e);
+    });    
+};
+
 Template.create.helpers({
     categories: function () {
         return Categories;
@@ -6,17 +48,18 @@ Template.create.helpers({
     items: function () {
         return Items;
     },
+
+    allItems: function () {
+        return Items.find();
+    },
     
-    location: function () {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            console.log(position.coords.latitude);
-            console.log(position.coords.longitude);
-        });
+    position: function () {
+        return Session.get("position");
     }
 });
 
 Template.create.events({
-    "click #addButton": function(e) {
+    "click #addButton": function (e) {
         e.preventDefault();
 
         var category = document.getElementById("category").value;
@@ -28,5 +71,17 @@ Template.create.events({
         };
 
         Items.insert(item);
+    },
+
+    "click #snapButton": function (e) {
+        e.preventDefault();
+
+        var video = document.getElementById("video");
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+
+        context.drawImage(video, 0, 0, 640, 480);
+
+        console.log("SNAPPED!");
     }
 });
